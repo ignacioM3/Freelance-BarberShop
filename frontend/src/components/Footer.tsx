209103abda0453d16eeca7926496bb68d1e58c31
ignacioM3 <ignacio.marquez159@gmail.com>
2025-05-30 00:14:50 -0300

    changes footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="bg-[#ae9961dd] shadow-[0_-2px_5px_rgba(0,0,0,0.3)] rounded-t-md mt-10 flex flex-col items-center justify-center text-center py-4 md:flex-row md:justify-between md:px-20 md:py-0">
         <Logo className="hidden w-[100px] md:block"/>
         <div className="text-md flex flex-col gap-2 mt-2">
-            <p>© 2024-2025 Buenos Aires Barbershop. Todos los derechos reservados.</p>
+            <p>© 2025-2026 Buenos Aires Barbershop. Todos los derechos reservados.</p>
         </div>
     </footer>
   )
